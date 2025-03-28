3f9b518223a30b51be50352ff2d081f26f0abda8
mgarper <nito141101@gmail.com>
2025-03-28 18:46:46 +0100

    completed modal

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-document.querySelector('.modal').card.style.display = 'none';
+document.querySelector('.modal').style.display = 'none';
 
-document.querySelector('#modal--selector').addEventListener('click', () => {
-  document.querySelector('.modal').card.style.display = 'block'
+document.querySelector('#modal--button').addEventListener('click', () => {
+  document.querySelector('.modal').style.display = 'block'
 })
 
-document.querySelector('.modal .btn--secondary').addEventListener('click', function() {
-  document.querySelector('.modal').card.style.display = 'none';
+document.querySelector('.modal__buttons .btn--secondary').addEventListener('click', function() {
+  document.querySelector('.modal').style.display = 'none';
+  console.log('Modal closed');
 });
 
-document.querySelector('.modal .btn--primary').addEventListener('click', function() {
+document.querySelector('.modal__buttons .btn--primary').addEventListener('click', function() {
     let modal = document.querySelector('.modal');
     modal.innerHTML = '<h1>Course completed</h1><p>All attenders will soon be notified</p>';
 });
